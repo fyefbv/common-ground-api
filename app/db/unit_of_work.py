5a243624420d7deecf2f2042388b2654013b79ef
Roman <roman.gavr10@gmail.com>
2025-10-07 15:58:07 +0300

    feat(db): добавлен паттерн Unit of Work для управления транзакциями
    - Реализован интерфейс и класс UnitOfWork для управления сессиями базы данных и транзакциями
    - Перенесена строка логгера в точке входа для корректного логирования при запуске приложения

---
+++ app/db/unit_of_work.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+
+from app.db.database import async_session_maker
+
+class IUnitOfWork(ABC):
+    """
+    Интерфейс для реализации паттерна Unit of Work
+    Определяет базовые методы для управления транзакциями в базе данных
+    """
+
+    @abstractmethod
+    async def __aenter__(self):
+        """Контекстный менеджер для входа в транзакцию"""
+
+        raise NotImplementedError
+
+    @abstractmethod
+    async def __aexit__(self, *args):
+        """Контекстный менеджер для выхода из транзакции"""
+
+        raise NotImplementedError
+
+    @abstractmethod
+    async def commit(self):
+        """Подтверждение транзакции"""
+
+        raise NotImplementedError
+
+    @abstractmethod
+    async def rollback(self):
+        """Откат транзакции"""
+
+        raise NotImplementedError
+
+class UnitOfWork(IUnitOfWork):
+    """
+    Реализация паттерна Unit of Work
+    Управляет сессиями базы данных и транзакциями
+    """
+
+    def __init__(self):
+        self.session_maker = async_session_maker
+
+    async def __aenter__(self):
+        self.session = self.session_maker()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        if exc_type:
+            await self.rollback()
+        await self.session.close()
+        self.session = None
+
+    async def commit(self):
+        await self.session.commit()
+
+    async def rollback(self):
+        await self.session.rollback()
--- app/main.py
+++ app/main.py
@@ -4,9 +4,8 @@ from fastapi import FastAPI
 from core.logger import app_logger
 
 
-app = FastAPI(title="CommonGround API")
-
-app_logger.info("🚀 Запуск приложения...")
+app = FastAPI(title="Common Ground API")
 
 if __name__ == "__main__":
+    app_logger.info("🚀 Запуск приложения...")
     uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
